0bd6bbf1c1dbf85b346fac5ec027a5913c31b48a
Morris Pogi <mjamontemayor@iskolarngbayan.pup.edu.ph>
2023-03-17 13:48:57 +0800

    report generation

--- src/components/Employee/StudentsWithDeficiencySearch.js
+++ src/components/Employee/StudentsWithDeficiencySearch.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import useStudentWithDeficiencyListStore from "../../hooks/useStudentWithDeficiencyListStore";
 import useDeficiencyNamesStore from "../../hooks/useDeficiencyNamesStore";
 import { useNavigate } from "react-router-dom";
+import { fetchGenerateReport } from "../../functions/employee";
 
 function StudentsWithDeficiencySearch() {
     const activeDeficiencyName = useDeficiencyNamesStore((state) => state.activeDeficiencyName);
@@ -30,17 +31,24 @@ function StudentsWithDeficiencySearch() {
     }, [formData]);
 
 
+    function donwloadReport() {
+        fetchGenerateReport(activeDeficiencyName.name);
+    }
+
+
     function searchStudents() {
         fetchStudentsWithDeficiency(activeDeficiencyName.name, formData.student_id, formData.student_name);
     }
 
     return ( <div className="addStudentSearch">
-    <span className="addStudentSearch_subtext">Enter Keyword:</span>
+    <span className="addStudentSearch_subtext">Enter:</span>
     <input placeholder="Student Number" name="student_id" className="standard_textinput" onChange={onChange}></input>
-    <span className="addStudentSearch_subtext">Enter Keyword:</span>
+    <span className="addStudentSearch_subtext">Enter:</span>
     <input placeholder="Name" name="student_name" className="standard_textinput" onChange={onChange}></input>
     <button className="blue_button" onClick={searchStudents}>Search</button>
-    <button className="red_button" onClick={() => {navigate("/AddStudent")}}>Add New Student</button>
+    <button className="red_button" onClick={() => {navigate("/AddStudent")}} >Add New Student</button>
+    <button className="green_button" onClick={donwloadReport}>Generate Report</button>
+    
 </div> );
 }
 

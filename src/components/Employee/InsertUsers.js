83eb79894a9d5faf753fa8dc30de719ed8e8eaf0
Morris Pogi <mjamontemayor@iskolarngbayan.pup.edu.ph>
2023-05-05 15:54:35 +0800

    Modal for upload

--- src/components/Employee/InsertUsers.js
+++ src/components/Employee/InsertUsers.js
@@ -3,10 +3,15 @@ import React, { useState } from 'react';
 import { fetchSubmitStudentList } from "../../functions/employee";
 import UploadedTable from "./UploadedTable";
 import useUploadListStore from "../../hooks/useUploadListStore";
+import AlertModal from '../Modals/AlertModal';
+import useAlertModalStore from "../../hooks/useAlertModalStore";
 
 function InsertUsers() {
     const [file, setFile] = useState(null);
     const fetchAllUploadedTasks = useUploadListStore((state) => state.fetchAllUploadedTasks);
+
+    const alertIsOpen = useAlertModalStore((state) => state.isOpen);
+    const openAlert = useAlertModalStore((state) => state.openAlert);
 
     function handleFileChange(event) {
         setFile(event.target.files[0]);
@@ -18,8 +23,10 @@ function InsertUsers() {
         try {
             const data = await fetchSubmitStudentList(file);
             console.log(data);
+            openAlert("Success", "Success", "Your file is being uploaded");
         } catch(error) {
             console.error(error);
+            openAlert("Error", "Error", "Something went wrong");
         }
 
         fetchAllUploadedTasks();
@@ -28,6 +35,7 @@ function InsertUsers() {
 
     return ( 
     <>
+        {alertIsOpen && <AlertModal /> }
         <div className="screenLayout">
             <EmployeeNav />
             <div className="title_contentDiv">
